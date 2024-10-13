01623d965741a57948b63cebaff02c0e5fd75906
Julisujo <julisujo@gmail.com>
2024-10-13 20:41:40 -0300

    Add files via upload

---
+++ DIETECH/db.py
@@ -0,0 +1,41 @@
+#db.py
+
+import os
+import sqlite3
+
+DIETECH_DB = os.path.join(os.path.dirname(__file__), "dietech.db")
+
+def get_db():
+    conn = sqlite3.connect(DIETECH_DB)
+    return conn
+
+def create_tables():
+    tables = [
+        """CREATE TABLE IF NOT EXISTS ingredientes (
+            nombre TEXT PRIMARY KEY,
+            calorias INTEGER NOT NULL,
+            tipo TEXT NOT NULL,
+            cantidad TEXT NOT NULL
+        )""",
+        """CREATE TABLE IF NOT EXISTS recetas (
+            nombre TEXT PRIMARY KEY,
+            ingredientes TEXT NOT NULL,
+            dificultad TEXT NOT NULL,
+            calorias INTEGER NOT NULL,
+            tiempo_coccion TEXT NOT NULL
+        )"""
+    ]
+    db = get_db()
+    cursor = db.cursor()
+    for table in tables:
+        cursor.execute(table)
+    db.commit()
+    db.close()
+
+"""CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL
+)"""
+
+
